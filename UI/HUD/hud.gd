fff7482e18a53c2a21a316836b2c52ccc9f62342
Deathwordknell <148710329+Deathwordknell@users.noreply.github.com>
2024-07-21 12:55:11 -0500

    Updated HUD

---
+++ UI/HUD/hud.gd
@@ -0,0 +1,9 @@
+extends CanvasLayer
+
+@onready var vial_select_1: TextureButton = %VialSelect1
+@onready var vial_select_2: TextureButton = %VialSelect2
+@onready var vial_select_3: TextureButton = %VialSelect3
+@onready var vial_select_4: TextureButton = %VialSelect4
+@onready var vial_select_5: TextureButton = %VialSelect5
+
+@onready var vial_panel_5: Panel = %VialPanel5
